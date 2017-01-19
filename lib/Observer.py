ed3053c96dc75f0759d3a414b783468b6bdc5cff
jeremy <a>
2017-01-19 11:20:53 +0100

    t

--- lib/Observer.py
+++ lib/Observer.py
@@ -4,6 +4,9 @@ from abc import ABCMeta, abstractmethod
 
 class Observer(metaclass=ABCMeta):
 
+    def __init__(self):
+        pass
+
     @abstractmethod
     def update(self, args):
         pass

fc347b02f1a26e08bb9f8b51d4b8787100ce2b89
jeremy <a>
2017-01-20 17:49:47 +0100

    debug

--- lib/Sensor.py
+++ lib/Sensor.py
@@ -19,7 +19,7 @@ class Sensor(Observable):
 
     # add an event for listen GPIO
     def add_event(self):
-        GPIO.add_event_detect(self.__channel, GPIO.BOTH, callback=self.update)
+        GPIO.add_event_detect(self.__channel, GPIO.BOTH, callback=self.update, bouncetime=200)
 
     # remove event of listen GPIO
     def remove_event(self):

72d6946aa4140af55dcdbf56ad023ec9dc86dfaf
jeremy <a>
2017-01-23 19:47:41 +0100

    test

--- lib/Sensor.py
+++ lib/Sensor.py
@@ -10,7 +10,7 @@ class Sensor(Observable):
     def __init__(self, channel):
         Observable.__init__(self)
         self.__channel = channel
-        GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     # callback observer
     def update(self, chan):
@@ -20,11 +20,11 @@ class Sensor(Observable):
     # add an event for listen GPIO
     def add_event(self, value):
         if value == 1:
-            GPIO.add_event_detect(self.__channel, GPIO.RISING, callback=self.update, bouncetime=300)
+            GPIO.add_event_detect(self.__channel, GPIO.RISING, callback=self.update, bouncetime=500)
         elif value == 0:
-            GPIO.add_event_detect(self.__channel, GPIO.FALLING, callback=self.update, bouncetime=300)
+            GPIO.add_event_detect(self.__channel, GPIO.FALLING, callback=self.update, bouncetime=500)
         else:
-            GPIO.add_event_detect(self.__channel, GPIO.BOTH, callback=self.update, bouncetime=300)
+            GPIO.add_event_detect(self.__channel, GPIO.BOTH, callback=self.update, bouncetime=500)
 
     # remove event of listen GPIO
     def remove_event(self):
